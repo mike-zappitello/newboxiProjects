4e88e5df2b66090f1556155ff998513551672e94
mzappitello <mzappitello@gmail.com>
2014-02-08 13:33:14 -0500

    added cod to get the current nba season.  working on script to get all the the .xml files from source

--- nbaProjects/stats/grabSingleGame.py
+++ nbaProjects/stats/grabSingleGame.py
@@ -1,42 +1,13 @@
 import urllib2
 import xml.etree.ElementTree as ET
-import MySQLdb as mdb
-import db
 
 # take in a players name and team and make sure we have an entry in the players table
 # return the index of the player
 def playerID(playerName):
-  con = db.connect_to_mysql()
-  if con:
-    try:
-      # some expression that checks for a player
-		  cur = con.cursor()
-      cur.execute('')
-      # todo - interpret the results from the mysql to get the ID
-    except mdb.Error, e:
-      print "Error %d: %s" % (e.args[0], e.args[1])
-    con.close()
 
 # take in a lineup and make sure we have an entry in the lineups table
 # retun the index of that lineup
 def lineupID(lineup):
-  con = db.connect_to_mysql()
-  if con:
-    try:
-      players = lineup.text.split(", ")
-      playerIds = []
-      for player in players:
-        playerIds.append(playerID(player))
-
-      playerIds.sort()
-
-      # some expression that checks for a lineup
-		  cur = con.cursor()
-      cur.execute('')
-      # todo - interpret the results from the mysql to get the ID
-    except mdb.Error, e:
-      print "Error %d: %s" % (e.args[0], e.args[1])
-    con.close()
 
 #log all of the data from the xml file into the db
 def logGame(file):
