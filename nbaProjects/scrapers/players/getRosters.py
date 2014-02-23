40a8ec80e046389a606467d89b7620b6c1dcae9f
mzappitello <mzappitello@gmail.com>
2014-02-22 19:29:52 -0500

    made a much better scraper for rosters that now puts players into the roster of the correct team in teams.json

--- nbaProjects/scrapers/players/getRosters.py
+++ nbaProjects/scrapers/players/getRosters.py
@@ -2,119 +2,124 @@ import urllib2
 from HTMLParser import HTMLParser
 import dataDirs as dataDir
 import json
+import dataDirs as dataDir
+import re
 
 # url containing roster data
 k_baseUrl = 'http://www.eskimo.com/~pbender/rosters.html'
+# teams file
+k_teamsFile = (dataDir.k_teamsDir + 'teams.json')
 
-# create subclass and override the handler methods?
-class rosterHTMLParser(HTMLParser):
-	def print_pre_contents(self, html):
-		self.tag_stack = ["dummy"]
-		self.roster_stack = []
-		self.feed(html)
-		return self.roster_stack
-
-	def handle_starttag(self, tag, attrs):
-		self.tag_stack.append(tag)
-
-	def handle_endtag(self, tag):
-		if len(self.tag_stack) > 0:
-			self.tag_stack.pop()
-
-	def handle_data(self, data):
-		if len(self.tag_stack) > 0 and  self.tag_stack[-1] == 'pre':
-			self.roster_stack.append(data)
-
-# get the html from the roster url and parse it out
-def parseRoster():
-  roster_page = urllib2.urlopen(k_baseUrl)
-  roster_page_html_shitty = roster_page.read()
-  roster_page.close()
-  roster_page_html = roster_page_html_shitty.replace("#", "")
-
-  theHTMLparser = rosterHTMLParser()
-  return theHTMLparser.print_pre_contents(roster_page_html)
-
-def teamsData():
-  teamsFileString = (dataDir.k_teamsDir + 'teams.json')
+# open up the teams json file and setup a json object of its data
+# we'll be writing our colors to theis json dict, and then 
+# resaving it to the file
+def getTeamData():
+  teamsFileString = (k_teamsFile)
   teamsFile = open(teamsFileString)
   teamData = json.load(teamsFile)
   teamsFile.close()
   return teamData['teams']
 
-def combineData(htmlData, jsonData):
-  leagueRoster = []
-  # for each team
-  for hTeam in htmlData:
-    print hTeam
-    # split the html team data by line and look at each line
-    teamArray = hTeam.split('\n')
-    for playerLine in teamArray:
-      # for this site, if a line conatains '...' its a platyer
-      if '...' in playerLine:
-        # split each line by a ' '
-        # 0 - player number
-        # 1 - first name
-        # 2 - last name
-        # 3 - "...." place holder
-        # 4 - position
-        # 5 - height
-        # 6 - weight
-        # 7 - birthday
-        # 8 - college
-        # 9 - experience
-        info = playerLine.split()
-        print info
-        if info[0] == "3?*Shawne":
-          player = {
-            'firstName' : "Shawne",
-            'lastName' : info[1],
-            'number' : "3",
-            'height' : info[4],
-            'weight' : info[5],
-            'position' : info[3],
-            'experience' : info[8],
-            'born' : info[5],
-            'college' : info[7]
-          }
-        elif info[1] == "Mirza":
-          player = {
-            'firstName' : info[1],
-            'lastName' : info[2],
-            'number' : info[0],
-            'height' : info[5],
-            'weight' : info[6],
-            'position' : info[4],
-            'experience' : "1",
-            'born' : info[6],
-            'college' : info[8]
-          }
-        elif info[1] == "Nene":
-          player = {
-            'firstName' : info[1],
-            'lastName' : "",
-            'number' : info[0],
-            'height' : info[4],
-            'weight' : info[5],
-            'position' : info[3],
-            'experience' : info[7],
-            'born' : info[5],
-            'college' : info[7]
-          }
-        else:
-          player = {
-            'firstName' : info[1],
-            'lastName' : info[2],
-            'number' : info[0],
-            'height' : info[5],
-            'weight' : info[6],
-            'position' : info[4],
-            'experience' : info[9],
-            'born' : info[6],
-            'college' : info[8]
-          }
-        leagueRoster.append(player)
-  return leagueRoster
+# open up the team json file
+# create a json string with our new team data
+# save and close the file
+def saveTeamData(teamData):
+  teamsFileString = (k_teamsFile)
+  teamsFile = open(teamsFileString, 'w')
+  newData = {'teams' : teamData}
+  newTeamsData = json.dumps(newData,sort_keys=True, indent=2, separators=(",", ":"))
+  teamsFile.write(newTeamsData)
+  teamsFile.close()
+
+# class to parse the k_baseUrl for teams
+class rosterHTMLParser(HTMLParser):
+  # parse and return the updated team data
+  def start_parse(self, html, teamData):
+    self.teamData = teamData
+    self.players = []
+    self.tag_stack = []
+    self.feed(html)
+    return self.teamData
+
+  # handles the starting tags
+  # if the tag is A then we have a new team and we call setCurrentTeam
+  # if the tag is PRE then we append it to our tag stack and will collect data
+  def handle_starttag(self, tag, attrs):
+    if (tag == "a" and attrs and attrs[0][0] == "name"):
+      self.setCurrentTeam(attrs[0][1])
+    elif (tag == "pre"):
+      self.tag_stack.append(tag)
+
+  # handles the end tags
+  # we only care about PRE, as its the only thing that will be in our tag stack
+  def handle_endtag(self, tag):
+    if self.tag_stack:
+      self.tag_stack.pop()
+
+  # if the tag stack has an element, its PRE, and we need to parse the data
+  def handle_data(self, data):
+    if self.tag_stack:
+      self.currentTeamData['roster'] = self.parseData(data)
+
+  # handle a data string
+  # for each line i throw it through some ugly regex
+  # if it passes we have a player and we create a dict for that player
+  # return a list of thoes player dicts
+  def parseData(self, data):
+    lines = data.split('\n')
+    players = []
+    for line in lines:
+      match = re.match(r"^\s?((?P<number>\d{1,2})|\?\?)(\s|\?|\*)*(?P<first_name>(\w|')*)\s?(?P<last_name>(\w|')*)\s*\.*\s*(?P<position>\w-?\w?)\s*(?P<height>\d-\d{1,2})\s*(?P<weight>\d{3})\s*(?P<birthday>\d{1,2}/\d{1,2}/\d{2})\s*(?P<the_rest>.*)", line)
+      if match:
+        player = {}
+        player['number'] = match.group('number')
+        player['first_name'] =  match.group('first_name')
+        player['last_name'] =match.group('last_name')
+        player['position'] = match.group('position')
+        player['height'] = match.group('height')
+        player['weight'] = match.group('weight')
+        player['birthday'] = match.group('birthday')
+
+        the_rest = match.group('the_rest').split()
+        player['experience'] = the_rest.pop()
+        college = ''
+        for word in the_rest:
+          college = college + word + ' '
+        player['college'] = college.rstrip()
+        players.append(player)
+
+    return players
+
+  # takes a team abbreviation from the website
+  # fixes the abbr for the four teams that have weird abbrevs
+  # searches through team list for the right abbrev
+  # set the current team data to that team
+  def setCurrentTeam(self, teamAbbr):
+    # fix for bad abbrs
+    if teamAbbr == 'pho':
+      teamAbbr = 'phx'
+    elif teamAbbr == 'uta':
+      teamAbbr = 'utah'
+    elif teamAbbr == 'bro':
+      teamAbbr = 'bkn'
+    elif teamAbbr == 'was':
+      teamAbbr = 'wsh'
+    for team in self.teamData:
+      if teamAbbr == team['abbr']:
+        self.currentTeamData = team
+        return
+
+# get the html from the roster url
+def getRosterHtml():
+  roster_page = urllib2.urlopen(k_baseUrl)
+  roster_page_html = roster_page.read()
+  roster_page.close()
+  # have to get rid of '&' symbols as it fucks things up
+  roster_page_clean = ''
+  for line in roster_page_html:
+    if line != "&":
+      roster_page_clean = roster_page_clean + line
+  return roster_page_html
 
 def saveData(dataAsList):
   jsonRoster = json.dumps(dataAsList, sort_keys=True, indent=2)
@@ -122,4 +127,8 @@ def saveData(dataAsList):
   jsonFile.write(jsonRoster)
   jsonFile.close()
 
-saveData(combineData(parseRoster(), teamsData()))
+# setup the parser, get the html, parse it, and save the new stuff
+parser = rosterHTMLParser()
+teamData = getTeamData()
+newData =  parser.start_parse(getRosterHtml(), teamData)
+saveTeamData(newData)
